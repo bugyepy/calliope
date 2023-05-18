e0815dacc9e56190fae031c630964f2714e727e3
YutaroTagami <yutaro.tagami@arsaga.jp>
2023-05-18 13:20:50 +0900

    add: albertを追加しタブで切り替えられるようにした

--- functions/albert_japanese.py
+++ functions/albert_japanese.py
@@ -7,10 +7,11 @@ tokenizer = AutoTokenizer.from_pretrained(
 model = AutoModelForMaskedLM.from_pretrained(
     "ken11/albert-base-japanese-v1-with-japanese-tokenizer")
 
-text = "名前は[MASK]です。"
-tokens = tokenizer(text, return_tensors="pt")
-mask_index = tokens["input_ids"][0].tolist().index(tokenizer.mask_token_id)
-predict = model(**tokens)[0]
-_, result = predict[0, mask_index].topk(5)
+def albert(input_text):
+    tokens = tokenizer(input_text, return_tensors="pt")
+    mask_index = tokens["input_ids"][0].tolist().index(tokenizer.mask_token_id)
+    predict = model(**tokens)[0]
+    _, result = predict[0, mask_index].topk(5)
 
-print(tokenizer.convert_ids_to_tokens(result.tolist()))
+    print(tokenizer.convert_ids_to_tokens(result.tolist()))
+    return tokenizer.convert_ids_to_tokens(result.tolist())
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import streamlit
-from functions import sentiment_analysis
+from functions import sentiment_analysis, albert_japanese
 
 streamlit.title("Calliope - LLM Sandbox")
 
-input_text = streamlit.text_area("下記の文章をもとに感情を判定します。", height=100)
+tab1, tab2 = streamlit.tabs(["感情分析", "ALBERT_TEST"])
 
-if streamlit.button("判定する"):
-    streamlit.write(sentiment_analysis.sentiment_analyzer(input_text))
+with tab1:
+    input_text = streamlit.text_area("下記の文章をもとに感情を判定します。", height=100, key=1)
+
+    if streamlit.button("判定する", key=3):
+        streamlit.write(sentiment_analysis.sentiment_analyzer(input_text))
+
+with tab2:
+    input_text = streamlit.text_area("下記の文章内の[MASK]という文字列が何か予測します。", height=100, key=2)
+    if streamlit.button("判定する", key=4):
+        if "[MASK]" in input_text:
+            streamlit.write(albert_japanese.albert(input_text))
+        else:
+            streamlit.write("[MASK]を含んだ文章を入力してください。")
